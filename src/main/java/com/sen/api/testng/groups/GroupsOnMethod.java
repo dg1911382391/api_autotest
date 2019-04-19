0c2e4f68790efee411c2e46faa7ca96be2a67aa4
dugang <1911382391@qq.com>
2019-04-19 17:32:33 +0800

    testng groups=groupName 组测试

---
+++ src/main/java/com/sen/api/testng/groups/GroupsOnMethod.java
@@ -0,0 +1,42 @@
+package com.sen.api.testng.groups;
+
+import org.testng.annotations.AfterGroups;
+import org.testng.annotations.BeforeGroups;
+import org.testng.annotations.Test;
+
+/**
+ * Created by dg on 2019-04-19.
+ */
+public class GroupsOnMethod {
+
+	@Test(groups = "server")
+	public void test1() {
+		System.out.println("这是服务端组的测试方法111");
+	}
+
+	@Test(groups = "server")
+	public void test2() {
+		System.out.println("这是服务端组的测试方法222");
+	}
+
+	@Test(groups = "client")
+	public void test3() {
+		System.out.println("这是客户端组的测试方法333");
+	}
+
+	@Test(groups = "client")
+	public void test4() {
+		System.out.println("这是客户端组的测试方法444");
+	}
+
+	@BeforeGroups("server")
+	public void beforeGroupsOnServer() {
+		System.out.println("这是服务端组运行之前运行的方法^^^");
+	}
+
+	@AfterGroups("server")
+	public void afterGroupsOnServer() {
+		System.out.println("这是服务端组运行之后运行的方法!!!");
+	}
+
+}
