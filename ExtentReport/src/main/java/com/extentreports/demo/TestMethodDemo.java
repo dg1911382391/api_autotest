8d840f6105ae0a0dfe8f117bed692d40b71487f9
dugang <1911382391@qq.com>
2019-04-23 15:31:10 +0800

    TestNG 断言及日志方法

--- ExtentReport/src/main/java/com/extentreports/demo/TestMethodDemo.java
+++ ExtentReport/src/main/java/com/extentreports/demo/TestMethodDemo.java
@@ -1,7 +1,34 @@
 package com.extentreports.demo;
 
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
 /**
+ * TestNG 断言及日志方法
  * Created by dg on 2019-04-23.
  */
 public class TestMethodDemo {
+
+	@Test
+	public void test1() {
+		Assert.assertEquals(1, 2);
+	}
+
+	@Test
+	public void test2() {
+		Assert.assertEquals(1, 1);
+	}
+
+	@Test
+	public void test3() {
+		Assert.assertEquals("aaa","aaa");
+	}
+
+	@Test
+	public void logDemo() {
+		Reporter.log("这是我们自己写的日志！");
+		throw new RuntimeException("这是我自己的运行时异常。");
+	}
+
 }
