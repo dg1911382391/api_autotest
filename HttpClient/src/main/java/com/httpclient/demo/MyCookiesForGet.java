86cb2ae4dd6fee071d2d7e5de243e55cf6caf6f8
dugang <1911382391@qq.com>
2019-04-25 17:03:04 +0800

    Mock HttpClient 通过配置文件优化方法

---
+++ HttpClient/src/main/java/com/httpclient/demo/MyCookiesForGet.java
@@ -0,0 +1,50 @@
+package com.httpclient.demo;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+/**
+ * Created by dg on 2019-04-25.
+ */
+public class MyCookiesForGet {
+
+	private String url;
+	private ResourceBundle bundle;
+
+	@BeforeTest
+	public void beforeTest() {
+		bundle = ResourceBundle.getBundle("application", Locale.CANADA);
+		url = bundle.getString("test.url");
+
+	}
+
+	@Test
+	public void getCookies() throws IOException {
+		String result;
+		/**
+		 * 从配置文件中拼接测试的URL
+		 */
+		String uri = bundle.getString("getCookies.uri");
+		String testUrl = this.url + uri;
+
+		/**
+		 * 测试逻辑代码
+		 */
+		HttpGet get = new HttpGet(testUrl);
+		DefaultHttpClient client = new DefaultHttpClient();
+		HttpResponse response = client.execute(get);
+
+		result = EntityUtils.toString(response.getEntity(), "utf-8");
+		System.out.println(result);
+
+	}
+
+}
