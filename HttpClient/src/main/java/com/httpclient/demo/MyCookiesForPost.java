c789fe5dca4fa2a878ce999cf7651a2f7bab533a
dugang <1911382391@qq.com>
2019-04-25 19:59:02 +0800

    Mock平台提供接口服务 HttpClient管理请求测试接口 携带Cookies信息的post请求

--- HttpClient/src/main/java/com/httpclient/demo/MyCookiesForPost.java
+++ HttpClient/src/main/java/com/httpclient/demo/MyCookiesForPost.java
@@ -3,9 +3,14 @@ package com.httpclient.demo;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.CookieStore;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.cookie.Cookie;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+import org.jsoup.helper.StringUtil;
+import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -17,7 +22,7 @@ import java.util.ResourceBundle;
 /**
  * Created by dg on 2019-04-25.
  */
-public class MyCookiesForGet {
+public class MyCookiesForPost {
 
 	private String url;
 	private ResourceBundle bundle;
@@ -70,42 +75,79 @@ public class MyCookiesForGet {
 	}
 
 	/**
-	 * 携带Cookies信息访问get请求
+	 * 携带Cookies信息访问post请求
 	 */
 	@Test(dependsOnMethods = {"testGetCookies"})
-	public void testGetWithCookies() throws IOException {
+	public void testPostWithCookies() throws IOException {
+		/**
+		 * 声明一个对象进行响应结果的存储
+		 */
 		String result;
+
 		/**
 		 * 从配置文件中拼接测试的URL
 		 */
-		String uri = bundle.getString("test.get.with.cookies");
+		String uri = bundle.getString("test.post.with.cookies");
 		String testUrl = this.url + uri;
 
 		/**
-		 * 测试逻辑代码
+		 * 声明一个DefaultHttpClient对象，用来进行方法的执行。
 		 */
-		HttpGet get = new HttpGet(testUrl);
 		DefaultHttpClient client = new DefaultHttpClient();
+
+		/**
+		 * 声明一个post方法
+		 */
+		HttpPost post = new HttpPost(testUrl);
+
+		/**
+		 * 添加参数
+		 */
+		JSONObject param = new JSONObject();
+		param.put("name", "zhangsan");
+		param.put("age", "18");
+
+		/**
+		 * 设置Headers请求头信息
+		 */
+		post.setHeader("content-type", "application/json");
+
+		/**
+		 * 将参数添加到方法中
+		 */
+		StringEntity entity = new StringEntity(param.toString(), "utf-8");
+		post.setEntity(entity);
 
 		/**
 		 * 设置Cookies信息
 		 */
 		client.setCookieStore(this.clientCookieStore);
 
-		HttpResponse response = client.execute(get);
 		/**
-		 * 获取响应状态码
+		 * 执行post方法
 		 */
-		int statusCode = response.getStatusLine().getStatusCode();
-		System.out.println("statusCode=" + statusCode);
-		if (statusCode == 200) {
-			result = EntityUtils.toString(response.getEntity(), "utf-8");
-			System.out.println(result);
-		}
+		HttpResponse response = client.execute(post);
+
+		/**
+		 * 获取响应结果
+		 */
+		result = EntityUtils.toString(response.getEntity(), "utf-8");
+		System.out.println(result);
+
+		/**
+		 * 处理结果，就是判断响应结果是否符合预期
+		 * 1、将返回的响应结果字符串String转化为json对象（最好别用alibaba.fastjson）
+		 * 2、获取并判断响应结果的值
+		 */
+		JSONObject resultJSON = new JSONObject(result);
+
+		String success = (String) resultJSON.get("zhangsan");
+		String status = (String) resultJSON.get("status");
+
+		Assert.assertEquals("succeed", success);
+		Assert.assertEquals("1", status);
 
 	}
-
-
 
 
 
