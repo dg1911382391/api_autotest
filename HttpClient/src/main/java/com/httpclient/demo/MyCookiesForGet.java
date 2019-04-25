6389ed5c1e175e99e3c29497d3ff3f1bab83903e
dugang <1911382391@qq.com>
2019-04-25 18:47:04 +0800

    Mock平台提供接口服务 HttpClient管理请求测试接口 携带Cookies信息的请求

--- HttpClient/src/main/java/com/httpclient/demo/MyCookiesForGet.java
+++ HttpClient/src/main/java/com/httpclient/demo/MyCookiesForGet.java
@@ -21,6 +21,10 @@ public class MyCookiesForGet {
 
 	private String url;
 	private ResourceBundle bundle;
+	/**
+	 * 用来存储cookies信息的变量
+	 */
+	CookieStore clientCookieStore;
 
 	@BeforeTest
 	public void beforeTest() {
@@ -29,8 +33,12 @@ public class MyCookiesForGet {
 
 	}
 
+	/**
+	 * 获取cookies信息
+	 * @throws IOException
+	 */
 	@Test
-	public void getCookies() throws IOException {
+	public void testGetCookies() throws IOException {
 		String result;
 		/**
 		 * 从配置文件中拼接测试的URL
@@ -51,13 +59,50 @@ public class MyCookiesForGet {
 		/**
 		 * 获取Cookies信息
 		 */
-		CookieStore clientCookieStore = client.getCookieStore();
+		this.clientCookieStore = client.getCookieStore();
 		List<Cookie> cookieList = clientCookieStore.getCookies();
 		for (Cookie cookie : cookieList) {
 			String cookieName = cookie.getName();
 			String cookieValue = cookie.getValue();
 			System.out.println("cookieName=" + cookieName + "；cookieValue=" + cookieValue);
 		}
+
+	}
+
+	/**
+	 * 携带Cookies信息的请求
+	 */
+	@Test(dependsOnMethods = {"testGetCookies"})
+	public void testGetWithCookies() throws IOException {
+		String result;
+		/**
+		 * 从配置文件中拼接测试的URL
+		 */
+		String uri = bundle.getString("test.get.with.cookies");
+		String testUrl = this.url + uri;
+
+		/**
+		 * 测试逻辑代码
+		 */
+		HttpGet get = new HttpGet(testUrl);
+		DefaultHttpClient client = new DefaultHttpClient();
+
+		/**
+		 * 设置Cookies信息
+		 */
+		client.setCookieStore(this.clientCookieStore);
+
+		HttpResponse response = client.execute(get);
+		/**
+		 * 获取响应状态码
+		 */
+		int statusCode = response.getStatusLine().getStatusCode();
+		System.out.println("statusCode=" + statusCode);
+		if (statusCode == 200) {
+			result = EntityUtils.toString(response.getEntity(), "utf-8");
+			System.out.println(result);
+		}
+
 
 	}
 
