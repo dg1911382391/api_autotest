e07b0c272439155d4011e03f7ab8b423d6c0262f
dugang <1911382391@qq.com>
2019-04-25 17:35:44 +0800

    Mock平台提供接口服务 HttpClient管理请求测试接口 获取Cookies信息

--- HttpClient/src/main/java/com/httpclient/demo/MyCookiesForGet.java
+++ HttpClient/src/main/java/com/httpclient/demo/MyCookiesForGet.java
@@ -1,13 +1,16 @@
 package com.httpclient.demo;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.client.CookieStore;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.cookie.Cookie;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
@@ -44,6 +47,17 @@ public class MyCookiesForGet {
 
 		result = EntityUtils.toString(response.getEntity(), "utf-8");
 		System.out.println(result);
+
+		/**
+		 * 获取Cookies信息
+		 */
+		CookieStore clientCookieStore = client.getCookieStore();
+		List<Cookie> cookieList = clientCookieStore.getCookies();
+		for (Cookie cookie : cookieList) {
+			String cookieName = cookie.getName();
+			String cookieValue = cookie.getValue();
+			System.out.println("cookieName=" + cookieName + "；cookieValue=" + cookieValue);
+		}
 
 	}
 
