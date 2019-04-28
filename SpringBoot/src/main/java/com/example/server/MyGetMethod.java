6ebcd4f5ac16fe1bc4a97972c1d5cba332beb96b
dugang <1911382391@qq.com>
2019-04-28 10:28:33 +0800

    springboot 开发要求客户端携带cookies、参数才能访问的接口

--- SpringBoot/src/main/java/com/example/server/MyGetMethod.java
+++ SpringBoot/src/main/java/com/example/server/MyGetMethod.java
@@ -1,11 +1,13 @@
 package com.example.server;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 /**
  * Created by dg on 2019-04-26.
@@ -13,6 +15,11 @@ import javax.servlet.http.HttpServletResponse;
 @RestController
 public class MyGetMethod {
 
+	/**
+	 * 给客户端返回cookies
+	 * @param response
+	 * @return
+	 */
 	@RequestMapping(value = "/getCookies", method = RequestMethod.GET)
 	public String getCookies(HttpServletResponse response) {
 		// HttpServletRequest	装请求信息的类
@@ -22,4 +29,56 @@ public class MyGetMethod {
 
 		return "恭喜你获得cookies信息成功！";
 	}
+
+	/**
+	 * 要求客户端携带cookies访问此接口
+	 * 这是一个需要携带cookies信息才能访问的get请求
+	 */
+	@RequestMapping(value = "getWithCookies", method = RequestMethod.GET)
+	public String getWithCookies(HttpServletRequest request) {
+		Cookie[] cookies = request.getCookies();
+		if (Objects.isNull(cookies)) {
+			return "你必须携带cookies信息来";
+		}
+		for (Cookie cookie : cookies) {
+			if (cookie.getName().equals("cookieName") && cookie.getValue().equals("cookieValue")) {
+				return "恭喜你访问成功";
+			}
+		}
+		return "需要携带cookies信息才能访问的get请求";
+	}
+
+	/**
+	 * 开发一个需要携带参数才能访问的get请求
+	 * 实现方式1：@RequestParam
+	 * URL:key=value&key=value
+	 * 如，http://localhost:8888/get/with/param?start=20&end=20
+	 * 模拟获取商品列表
+	 */
+	@RequestMapping(value = "/get/with/param", method = RequestMethod.GET)
+	public Map<String, Integer> getList(@RequestParam Integer start, @RequestParam Integer end) {
+		Map<String, Integer> shopListMap = new HashMap<>();
+		shopListMap.put("鞋", 500);
+		shopListMap.put("干脆面", 1);
+		shopListMap.put("衬衫", 300);
+		return shopListMap;
+	}
+
+	/**
+	 * 开发一个需要携带参数才能访问的get请求
+	 * 实现方式2：@PathVariable
+	 * URL:ip/port/get/with/param/10/20
+	 * 如，http://localhost:8888/get/with/param/10/20
+	 * 模拟获取商品列表
+	 */
+	@RequestMapping(value = "/get/with/param/{start}/{end}")
+	public Map<String, Integer> getList2(@PathVariable Integer start, @PathVariable Integer end) {
+		Map<String, Integer> shopListMap = new HashMap<>();
+		shopListMap.put("鞋", 500);
+		shopListMap.put("干脆面", 1);
+		shopListMap.put("衬衫", 940);
+		return shopListMap;
+	}
+
+
 }
